842f6ee9beda2c4bf26a495358916edf2913c3ba
joao <jvd.ramos@hotmail.com>
2021-09-06 11:18:58 -0400

    ajuste para uso de rotas

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -3,14 +3,21 @@ import {View, Text} from 'react-native';
 import {styles} from './styles';
 import {TextIn} from '../../components/TextInput';
 import {ButtonLogin} from '../../components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export function Login() {
+  const navigation = useNavigation();
+  
+  function handleLogin(){
+    navigation.navigate('Home')
+  }
+
   return (
     <View style={styles.container}>
       <Text styles={styles.title}>Welcome</Text>
       <TextIn example="Login" />
       <TextIn example="Password" contentSecure />
-      <ButtonLogin color="#f05" title="Acessar" />
+      <ButtonLogin color="#f05" title="Acessar" onPress={()=>navigation.navigate('Home')}/>
     </View>
   );
 }
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 import {styles} from './styles';
 
 export function ButtonLogin(props) {
+
+  const navigation = useNavigation();
+  
+  function handleLogin(){
+    navigation.navigate('Home')
+  }
+
   return (
-    <Button title={props.title} style={styles.container} color={props.color}>
+    <Button title={props.title} style={styles.container} color={props.color} onPress={handleLogin}>
       <Text>{}</Text>
     </Button>
   );
