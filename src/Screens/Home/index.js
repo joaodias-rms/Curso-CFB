f001940144aa85488d9c6c9dc65242c95df6ba35
joao <jvd.ramos@hotmail.com>
2021-09-06 11:06:34 -0400

    Criado componentes base

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function Home() {
+export function Home() {
     return (
         <View>
-            <Text></Text>
+            <Text>Seja bem vindo</Text>
         </View>
     )
 }
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,18 +1,11 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { styles } from './styles'
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {styles} from './styles';
 
 export function ButtonLogin(props) {
-    const navigation = useNavigation()
-
-function handleLogin(){
-    navigation.navigate("Home")
-}
-
-    return (
-        <Button onPress={handleLogin} style={styles.container} color={props.color} >
-            <Text>{props.title}</Text>
-        </Button>
-    )
+  return (
+    <Button title={props.title} style={styles.container} color={props.color}>
+      <Text>{}</Text>
+    </Button>
+  );
 }
