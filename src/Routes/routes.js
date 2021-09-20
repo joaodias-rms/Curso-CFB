b137c16f36a77ab2b220f8826ad73f371e53b8f4
joao <jvd.ramos@hotmail.com>
2021-09-20 16:08:01 -0400

    criação de rotas

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import {
-  createStackNavigator,
-} from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import {Home} from '../Screens/Home/index';
 import {Login} from '../Screens/Login/index';
+import {SignUp} from '../Screens/SignUp';
+import {SplashScreen} from '../Screens/SplashScreen';
 
 export function Navigation() {
   const Nav = createStackNavigator();
 
   return (
-    <Nav.Navigator initialRouteName="Login">
-      <Nav.Screen name="Login" component={Login} options={{title: 'Welcome'}} />
-      <Nav.Screen name="Home" component={Home} options={{title: 'Home'}} />
+    <Nav.Navigator initialRouteName="SplashScreen" headerShow={false}>
+      <Nav.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{title: 'Welcome'}}
+      />
+      <Nav.Screen 
+        name="Login" 
+        component={Login} 
+        options={{title: 'Welcome'}} />
+      <Nav.Screen 
+        name="Home" 
+        component={Home} 
+        options={{title: 'Home'}} />
+      <Nav.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{title: 'SignUp'}}
+      />
     </Nav.Navigator>
   );
 }
