627f2220bfc995637b2dc59b579c734ffc95a9ba
joao <jvd.ramos@hotmail.com>
2021-09-20 16:08:13 -0400

    estilização de componentes

---
+++ src/Screens/SplashScreen/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#63c2d1',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
